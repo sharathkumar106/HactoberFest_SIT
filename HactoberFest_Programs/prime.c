157ea21dca03d6cddcf56e8f1a872b6605e77ff6
Shravyakulal14 <52647098+Shravyakulal14@users.noreply.github.com>
2019-10-26 01:26:20 -0400

    Update prime.c

--- HactoberFest_Programs/prime.c
+++ HactoberFest_Programs/prime.c
@@ -1,27 +1,27 @@
 #include<stdio.h>
 #include<stdlib.h>
 int main(){
-	int i,num,flag=0,count=0,start,end;
+	int i,num,flag=1,count=0,start,end;
 	printf("\nEnter starting number: ");
 	scanf("%d",&start);
 	printf("\nEnter ending number: ");
 	scanf("%d",&end);
 	for(num=start;num<end;num++){
-		if (num==0||num==1){
-			flag=1;
+		if (num==0||num==1)
 			continue;
-		}
-		for(i=2;i<num/2;i++){
+			
+		
+		for(i=2;i<=num/2;i++){
 			if(num%i==0){
-				flag=1;
+				flag=0;
 				break;
 			}
 		}
-		if(flag==0){
+		if(flag==1){
 			count++;
 		}
 		else{
-			flag=0;
+			flag=1;
 		}
 		
 	}
