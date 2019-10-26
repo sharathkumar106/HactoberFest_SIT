db26b869413b4bee6baa869718a8cb96104fc998
20051998-shwetha <52647030+20051998-shwetha@users.noreply.github.com>
2019-10-26 10:14:10 +0530

    Update fact.c

--- HactoberFest_Programs/fact.c
+++ HactoberFest_Programs/fact.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
 int fact(int num){
-	if(num==1)
+	if(num==0)
+		return 0;
+	else if(num==1)
 		return 1;
 	else
-		return num* fact(num-1);
+		return num*fact(num-1);
 }
 int main(){
 	int num;
