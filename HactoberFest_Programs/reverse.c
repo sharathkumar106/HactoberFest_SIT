f41d452e3ed1420da64d516d8dcef904e6987f63
vimaleshn98 <52690867+vimaleshn98@users.noreply.github.com>
2019-10-26 10:28:55 +0530

    Update reverse.c

--- HactoberFest_Programs/reverse.c
+++ HactoberFest_Programs/reverse.c
@@ -2,32 +2,22 @@
 #include<string.h>
 
 int main(){
-	int size=0,i,flag=1;
 	char str[50];
 	do{
 		
 	printf("\nEnter a string: ");
 	gets(str);
-	i=0;
-	while(str[i]!='\0'){
-		i++;
-	}
-	size=i-1;
-	for(i=0;i<=size/2;i++){
-		if(str[i]!=str[size-i]){
-			flag=0;
-			break;
-		}
-		else{
-			flag=1;
-		}
-		printf("\n%c\t%c",str[i],str[size-i]);
-	}
-	if(flag==1){
-		printf("\nPalindrome");
-	}
-	else{
-		printf("\nNot Palindrome");
-	}
-	}while(str!="end");
+        // Start from leftmost and rightmost corners of str 
+        int l = 0; 
+        int h = strlen(str) - 1; 
+  
+        // Keep comparing characters while they are same 
+        while (h > l) 
+        { 
+           if (str[l++] != str[h--]) 
+           { 
+              printf("%s is Not Palindrome", str); 
+           } 
+        } 
+        printf("%s is palindrome", str);
 }	
